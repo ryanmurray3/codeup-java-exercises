1aeaac1d7a5a2bb8fa19ba7a087645bcee6e513b
RyanMurray <ryangmurray3@gmail.com>
2023-10-31 11:57:56 -0500

    1 thru 10 done

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,6 +3,24 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!");
 
+
+       int myFavoriteNumber = 333;
+        System.out.println(myFavoriteNumber);
+
+        String myString = "can be empty? Yes...will it be useful in the future?";
+        System.out.println(myString);
+
+        float myNumber = 3;
+        System.out.println(myNumber);
+
+        int x = 5;
+        System.out.println(x++);
+        System.out.println(x);
+
+        int x = 5;
+        System.out.println(++x);
+        System.out.println(x);
+
     }
 
 }
