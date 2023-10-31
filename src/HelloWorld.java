4586d9cd63cce4873d4c226945cb6ab57cfe776b
RyanMurray <ryangmurray3@gmail.com>
2023-10-31 12:20:03 -0500

    finished 1 thru 14 of the exercise

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -22,7 +22,25 @@ public class HelloWorld {
         System.out.println(x);
         String myExplainWhy = "The code above works as an increment after the number adding the number by like x + 1...The other code does not work because I am trying to declare a variable in the same scope.  In java I cannot declare a variable with the same name in the same scope.  So, if I comment out the first set of code the ++x code executes.";
 
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o;
 
+//        int three = (int) "three";
+
+        int xY = 4;
+        x += 5;
+
+        int x = 3;
+        int y = 4;
+        y *= x;
+
+        int x = 10;
+        int y = 2;
+        x /= y;
+        y -= x;
+
+        String problem14 = "If I assign a variable to a numerical variable that is larger or smaller than the type it can hold I receive a red warning of a lossy result.  If I increment a numeric variable past the type's capacity, it results in an overflow." + "the example takes 4 bytes of data.";
     }
 
 }
