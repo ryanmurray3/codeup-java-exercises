e0a5c26ba6a3fbade557be7f2b5d7351d224fdca
RyanMurray <ryangmurray3@gmail.com>
2023-10-31 11:42:12 -0500

    java intro completed

---
+++ src/HelloWorld.java
@@ -0,0 +1,19 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+//all java is in classes and written in upper camel case or Pascal Case
---
+++ src/SyntaxLecture.java
@@ -0,0 +1,21 @@
+import java.sql.SQLOutput;
+
+public class SyntaxLecture {
+    public static void main(String[] args) {
+
+
+        String animal = "Dog";
+        String breed = "ShibaDoodle";
+        int weight = 70;
+        boolean cuteOrNot = false;
+        cuteOrNot = false;
+        String name = "MonsterTurtle";
+        System.out.println(name + " is a " + breed);
+        if (cuteOrNot) {
+            System.out.println(name + " is cute ");
+            } else {
+                System.out.println(name + " is mean lookin");
+            }
+        }
+    }
+
