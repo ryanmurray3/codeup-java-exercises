cc52cc18a65e17696a48a39b96e72ee0450de4c4
RyanMurray <ryangmurray3@gmail.com>
2023-11-06 15:42:52 -0600

    exercises are done for circle and circleApp

---
+++ src/util/Person.java
@@ -0,0 +1,40 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Person {
+    private String name;
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    public String getName(){
+        return this.name;
+//TODO: return the person's name
+    }
+    public void setName(String name) {
+        this.name = name;
+//TODO: change the name field to the passed value
+    }
+        public void sayHello () {
+            System.out.println("Hello " + this.name);
+//TODO: print a message to the console using the person's name
+        }
+
+    public static void main(String[] args) {
+        Person person1 = new Person("John");
+        Person person2 = person1;
+        System.out.println(person1.getName());
+        System.out.println(person2.getName());
+        person2.setName("Jane");
+        System.out.println(person1.getName());
+        System.out.println(person2.getName());
+//        Person person = new Person("John");
+//        System.out.println(person.getName());
+//        person.setName("Jane");
+//        System.out.println(person.getName());
+//        person.sayHello();
+    }
+
+
+}
